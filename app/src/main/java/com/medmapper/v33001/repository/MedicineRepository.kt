a819e5d4cdc3568eb8bc0a4741084862fd5f9293
loganfarwick <lflttd21@gmail.com>
2023-02-16 14:15:49 -0500

    Updated build.gradle(s) to include Room and other dependencies necessary for implementation. Created a medicine dto, MedicineDAO interface, and other functionality(ies) of Room.

---
+++ app/src/main/java/com/medmapper/v33001/repository/MedicineRepository.kt
@@ -0,0 +1,24 @@
+package com.medmapper.v33001.repository
+
+import androidx.annotation.WorkerThread
+import com.medmapper.v33001.dao.MedicineDAO
+import com.medmapper.v33001.dto.Medicine
+import kotlinx.coroutines.flow.Flow
+
+// Declares teh DAO as private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class MedicineRepository(private val medicineDAO: MedicineDAO) {
+
+    // Room executes all queries on a separate thread.
+    // Observed Flow will notify the observer when the data has changed.
+    val allMedicines: Flow<List<Medicine>> = medicineDAO.getAlphabetizedMedicine()
+
+    // By default Room runs suspend queries off the main thread, therefore
+    // we don't need to implement anything else to ensure we're not doing
+    // long running database work off the main thread
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(medicine: Medicine) {
+        medicineDAO.insert(medicine)
+    }
+}
