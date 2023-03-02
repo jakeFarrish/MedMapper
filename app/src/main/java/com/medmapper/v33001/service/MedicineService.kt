e76a7cdf24d35624059ec7e89e7fe620b353a2a1
chercher20 <93340674+chercher20@users.noreply.github.com>
2023-03-02 18:50:37 -0500

    Created a service package

---
+++ app/src/main/java/com/medmapper/v33001/service/MedicineService.kt
@@ -0,0 +1,21 @@
+package com.medmapper.v33001.service
+
+import com.medmapper.v33001.dao.MedicineDAO
+import com.medmapper.v33001.dto.Medicine
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+
+interface IMedicineService{
+    suspend fun fetchMedicines() : List<Medicine>?
+}
+abstract class MedicineService: IMedicineService {
+    suspend fun fetcMedicines() : List<Medicine>? {
+        return withContext(Dispatchers.IO) {
+            val service = RetrofitClientInstance.retrofitInstance?.create(MedicineDAO::class.java)
+            val medicines = async {service?.getAllMedicines()}
+            var result = medicines.await()?.awaitResponse()?.body()
+            return@withContext result
+        }
+    }
+}
