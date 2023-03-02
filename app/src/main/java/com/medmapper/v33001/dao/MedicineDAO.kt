c52c6160ea52ff99f66a5bb3bbcd4c9d3f276dfd
loganfarwick <lflttd21@gmail.com>
2023-03-02 16:42:46 -0500

    Modified functions within the MedicineDAO and added inline comments

--- app/src/main/java/com/medmapper/v33001/dao/MedicineDAO.kt
+++ app/src/main/java/com/medmapper/v33001/dao/MedicineDAO.kt
@@ -6,12 +6,16 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MedicineDAO {
+
+    // Query SQL for the Room Database
     @Query("SELECT * FROM Medicine ORDER BY name ASC")
     fun getAlphabetizedMedicine(): Flow<List<Medicine>>
 
+    // Insert SQL for the Room Database
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(medicine: Medicine)
 
-    @Query("DELETE FROM Medicine")
-    suspend fun deleteAll()
+    // Delete SQL for the Room Database
+    @Query("DELETE medicine FROM Medicine")
+    suspend fun delete()
 }
