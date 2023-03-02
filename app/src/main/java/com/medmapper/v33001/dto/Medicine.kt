64c0d1cc17c738ea043b0749761a2d5bf03ac1bf
loganfarwick <lflttd21@gmail.com>
2023-03-02 16:55:16 -0500

    Modified Room database structure and added inline comments

--- app/src/main/java/com/medmapper/v33001/dto/Medicine.kt
+++ app/src/main/java/com/medmapper/v33001/dto/Medicine.kt
@@ -8,8 +8,12 @@ import androidx.room.PrimaryKey
 data class Medicine(
     @PrimaryKey(autoGenerate = true) val medID: Int,
     @ColumnInfo(name = "name") val name: String?,
-    @ColumnInfo(name = "strength") val strength: String?,
+    @ColumnInfo(name = "quantity") val quantity: String?,
     @ColumnInfo(name = "start date") val startDate: String?,
-    @ColumnInfo(name = "prescription length") val lengthInDays: Int
+    @ColumnInfo(name = "prescription length") val lengthInDays: Int,
     //,@ColumnInfo() val endDate: Date = startDate
+    // Haven't finished the proper calculation of endDate value for medicine
+    @ColumnInfo(name = "take time") val takeTime: String?
+
+    // Above ^^ is the structure of the Room database with the different column information and expected values/data types
 )
