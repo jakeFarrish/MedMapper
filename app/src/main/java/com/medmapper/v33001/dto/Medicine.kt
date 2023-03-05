d7703e9afd4b9fe3303efbfdda6858974c57cd58
motakean <122765619+motakean@users.noreply.github.com>
2023-03-05 15:38:44 -0500

    Update Medicine.kt
    Used kdot documentation to better understand what the data class is functioning as.
    source: https://kotlinlang.org/docs/kotlin-doc.html#block-tags

--- app/src/main/java/com/medmapper/v33001/dto/Medicine.kt
+++ app/src/main/java/com/medmapper/v33001/dto/Medicine.kt
@@ -3,7 +3,15 @@ package com.medmapper.v33001.dto
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+/**
+ * A data class representing Medicine
+ *
+ * @property name; the name of the medicine
+ * @property strength; the strength of the medicine
+ * @property start_date; the date the patient started the medicine
+ * @property preciscription_length; how long the patient should take the medicine
+ *    
+ */
 @Entity(tableName = "Medicine")
 data class Medicine(
     @PrimaryKey(autoGenerate = true) val medID: Int,
