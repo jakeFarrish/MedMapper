be5579e8a4999bd00838c4e5ac5a21dd9cc77647
wryther <rytherwy@mail.uc.edu>
2023-03-05 19:02:54 -0500

    Corrected spelling, replaced top comments with Kdoc

--- app/src/main/java/com/medmapper/v33001/repository/MedicineRepository.kt
+++ app/src/main/java/com/medmapper/v33001/repository/MedicineRepository.kt
@@ -5,17 +5,21 @@ import com.medmapper.v33001.dao.MedicineDAO
 import com.medmapper.v33001.dto.Medicine
 import kotlinx.coroutines.flow.Flow
 
-// Declares teh DAO as private property in the constructor. Pass in the DAO
-// instead of the whole database, because you only need access to the DAO
+/**
+ * Declares the DAO as private property in the constructor.
+ * Pass in the DAO instead of the whole database, because you only need access to the DAO
+ */
 class MedicineRepository(private val medicineDAO: MedicineDAO) {
 
-    // Room executes all queries on a separate thread.
-    // Observed Flow will notify the observer when the data has changed.
-    val allMedicines: Flow<List<Medicine>> = medicineDAO.getAlphabetizedMedicine()
+     // Room executes all queries on a separate thread.
+     // Observed Flow will notify the observer when the data has changed.
 
-    // By default Room runs suspend queries off the main thread, therefore
-    // we don't need to implement anything else to ensure we're not doing
-    // long running database work off the main thread
+    val allMedicines: Flow<List<Medicine>> = medicineDAO.getAllAlphabetizedMedicine()
+
+     // By default Room runs suspend queries off the main thread, therefore
+     // we don't need to implement anything else to ensure we're not doing
+     // long running database work off the main thread
+
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
     suspend fun insert(medicine: Medicine) {
