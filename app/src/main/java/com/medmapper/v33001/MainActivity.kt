e9f87f070c38bb17f2c191161cf0cec498d78967
bonima21 <79886034+bonima21@users.noreply.github.com>
2023-03-25 14:33:23 -0400

    Creating outline textField

--- app/src/main/java/com/medmapper/v33001/MainActivity.kt
+++ app/src/main/java/com/medmapper/v33001/MainActivity.kt
@@ -8,14 +8,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.medmapper.v33001.dto.User
 import com.medmapper.v33001.ui.theme.MedMapperTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class MainActivity : ComponentActivity() {
+
+    private var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+    private val viewModel: MainViewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            firebaseUser?.let {
+                val user = User(it.uid, "")
+                viewModel.user = user
+                viewModel.listenToMedicine()
+            }
+            val medicine by viewModel.medicine.observeAsState(initial = emptyList())
             MedMapperTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -30,8 +48,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SpecimenFacts(name:String) {
+    var medicineName by remember {mutableStateOf("")}
+    var prescriptionStrength by by remember{mutableStateOf( "")}
+    var startDate by remember{mutableStateOf( "")}
+    var intakeFrequency by remember{mutableStateOf("")}
+    var prescriptionLength by remember{mutableStateOf("")}
 }
 
 @Preview(showBackground = true)
